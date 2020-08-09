7eff63cfe3e2cbc595907cd13e177578ed8bb9e9
kelvin53 <geshkoskei53@gmail.com>
2020-08-09 13:20:46 +0300

    define imagedetails function in views

--- Instaclone/views.py
+++ Instaclone/views.py
@@ -12,3 +12,13 @@ def index(request):
 
 
     return render(request,'index.html',{"photos":photos,"profiles":profiles,"comments":comments})
+
+def imagedetails(request,image_id):
+    comments=Comment.objects.filter(image_id=image_id)
+
+
+    try:
+        image = Image.objects.get(id=image_id)
+    except ObjectDoesNotExist:
+         raise Http404()
+    return render(request,"image.html",{"image":image,"comments":comments})
