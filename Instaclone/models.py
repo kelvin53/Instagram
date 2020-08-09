9e8dc7313afab081cfc48a25b3a329f66952eac5
kelvin53 <geshkoskei53@gmail.com>
2020-08-09 13:35:43 +0300

    add image class to models file

--- Instaclone/models.py
+++ Instaclone/models.py
@@ -27,3 +27,36 @@ class Profile(models.Model):
     def search_by_user(cls,search_term):
         instagram=cls.objects.filter(user__username=search_term)
         return instagram
+
+class Image(models.Model):
+    image=models.ImageField(upload_to ='images/', blank = True)
+    name=models.CharField(max_length =30)
+    caption=models.TextField(blank=True,null=True)
+    profile=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    pub_date=models.DateTimeField(auto_now_add=True,blank=True,null=True)
+    likes=models.IntegerField(blank=True,null=True)
+    # comments=models.ForeignKey(Comment)
+
+    # def __str__(self):
+    #     return self.name
+
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()
+
+
+    def update_caption(self,caption):
+        self.caption = caption
+        self.save()
+
+
+    @classmethod
+    def get_photos(cls):
+        photos=cls.objects.all()
+        return photos
+
+    @classmethod
+    def get_image_by_id(cls,image_id):
+        image=cls.objects.get(id=image_id)
+        return image
