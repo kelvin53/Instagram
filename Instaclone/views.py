8ea9107a07bf4842f47fe5a363d21d7d9e1b1674
kelvin53 <geshkoskei53@gmail.com>
2020-08-09 13:04:18 +0300

    define index in views

--- Instaclone/views.py
+++ Instaclone/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    current_user=request.user
+
+    photos=Image.get_photos()
+    comments=Comment.get_comments()
+    # profile = Profile.objects.get(user=current_user)
+    profiles = Profile.objects.all()
+
+
+    return render(request,'index.html',{"photos":photos,"profiles":profiles,"comments":comments})
