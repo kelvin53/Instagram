8479f51e3ea866bf2384018e264218b8c289bad4
kelvin53 <geshkoskei53@gmail.com>
2020-08-09 16:26:31 +0300

    update settings

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'Instaclone.apps.InstacloneConfig',
     'tinymce',
+    'bootstrap3'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
