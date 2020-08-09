20a28ef5d1f2eb514575e8de77e681ad7f203535
kelvin53 <geshkoskei53@gmail.com>
2020-08-09 16:13:55 +0300

    update settings/urls.py

--- Instaclone/models.py
+++ Instaclone/models.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.db import models
 import datetime as dt
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
--- Instagram/urls.py
+++ Instagram/urls.py
@@ -19,7 +19,7 @@ from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('instagram.urls')),
+    url(r'',include('Instaclone.urls')),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
     url(r'^tinymce/',include('tinymce.urls')),
